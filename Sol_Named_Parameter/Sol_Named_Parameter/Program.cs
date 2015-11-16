9e4d163354db7e605c653d912f76c9876a7afc9a
KishorNaik <kishor.naik011.net@gmail.com>
2015-11-16 21:55:55 +0530

    # Commit..

---
+++ Sol_Named_Parameter/Sol_Named_Parameter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sol_Named_Parameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetValues(MiddleName: "Chandrakant", LastName: "Naik", FirstName: "Kishor");
+        }
+
+        public static void GetValues(String FirstName, String MiddleName, String LastName)
+        {
+            System.Console.WriteLine("{0} {1} {2}",FirstName,MiddleName,LastName);
+            
+        }
+    }
+}
